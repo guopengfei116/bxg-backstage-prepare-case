616394349cfb208ce97f337375e7d72dd3d22168
PengFei <626138959@qq.com>
2017-09-10 02:02:37 +0800

    个人中心与密码修改

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -1,1 +1,39 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+
+/**
+ * 数据回显：
+ * 1、请求接口获取数据
+ * 2、通过模版引擎获取数据渲染后的模版，然后插入到页面指定位置
+ * */
+$.get('/v6/teacher/profile', function(data) {
+  if(data.code == 200) {
+    $('.teacher-profile').html(template('teacher-profile-tpl', data.result));
+  }
+});
+
+/**
+ * 表单提交：
+ * 1、因为数据要回写，造成表单是动态生成的，所以要委托的方式监听表单submit事件
+ * 2、提交数据前需要把省市县三个值合成一个tc_hometown字段，合成后的数据格式为：省|市|县
+ * 3、然后把合成后的
+ * */
+$(document).on('submit', '#settings-form', function(e) {
+  e.preventDefault();
+
+  // 把合成后省市县动态设入tc_hometown字段对应的表单
+  var hometown =
+    $('.hometown select').map(function() {
+      return $(this).find('option:selected').text();
+    })
+    .toArray().join('|');
+  $('#input-hometown').val(hometown);
+
+  // ajax提交
+  $(this).ajaxSubmit(function(data) {
+    if(data.code == 200) {
+      alert('修改成功');
+    }
+  });
+});
--- src/js/user/repass.js
+++ src/js/user/repass.js
@@ -1,1 +1,31 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+
+/**
+ * 修改密码：
+ * 1、监听表单提交事件，阻止默认的刷新提交
+ * 2、校验新密码与确认密码
+ * 3、不通过终止提交，通过转ajax方式提交
+ * 4、密码修改成功后退出并跳转到登陆页
+ * */
+$('#repass-form').on('submit', function() {
+
+  // 校验
+  var newPass = $('#input-pass').val();
+  var newPassRepeat = $('#input-pass-repeat').val();
+  if(newPass !== newPassRepeat) {
+    alert('两次密码输入不一致');
+    return false;
+  }
+
+  // ajax提交，成功后触发头部的退出功能
+  $(this).ajaxSubmit(function(data) {
+    if(data.code == 200) {
+      alert('密码修改成功');
+      $('#btn-logout').trigger('click');
+    }
+  });
+
+  return false;
+});
