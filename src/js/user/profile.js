eeede3bebf15685f4c8634f79bd9fc074d6e0f33
PengFei <626138959@qq.com>
2017-09-11 13:21:26 +0800

    调整个人中心与密码修改实现

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -16,24 +16,26 @@ $.get('/v6/teacher/profile', function(data) {
 /**
  * 表单提交：
  * 1、因为数据要回写，造成表单是动态生成的，所以要委托的方式监听表单submit事件
- * 2、提交数据前需要把省市县三个值合成一个tc_hometown字段，合成后的数据格式为：省|市|县
- * 3、然后把合成后的
+ * 2、提交数据前需要把省市县合成为(省|市|县)这样的数据格式，并通过tc_hometown字段传给后端
+ * 3、修改成功后给出用户提示
  * */
-$(document).on('submit', '#settings-form', function(e) {
-  e.preventDefault();
+$('#settings-form').ajaxForm({
+  delegation: true,
 
-  // 把合成后省市县动态设入tc_hometown字段对应的表单
-  var hometown =
-    $('.hometown select').map(function() {
-      return $(this).find('option:selected').text();
-    })
-    .toArray().join('|');
-  $('#input-hometown').val(hometown);
+  beforeSubmit: function(arr, $form, options) {
 
-  // ajax提交
-  $(this).ajaxSubmit(function(data) {
+    // 合成(省|市|县)，添加tc_hometown字段
+    var hometown = $('.hometown select')
+      .map(function() {
+        return $(this).find('option:selected').text();
+      }).toArray().join('|');
+
+    arr.push({ name: 'tc_hometown', value: '河北|张家口' });
+  },
+
+  success: function(data) {
     if(data.code == 200) {
       alert('修改成功');
     }
-  });
+  }
 });
--- src/js/user/repass.js
+++ src/js/user/repass.js
@@ -4,28 +4,28 @@ require('../common/aside.js');
 
 /**
  * 修改密码：
- * 1、监听表单提交事件，阻止默认的刷新提交
- * 2、校验新密码与确认密码
- * 3、不通过终止提交，通过转ajax方式提交
- * 4、密码修改成功后退出并跳转到登陆页
+ * 1、使用插件ajaxForm方法把表单刷新提交转ajax提交
+ * 2、配置提交前的钩子函数，校验新密码与确认密码，不一致return false终止提交
+ * 3、配置成功回调，修改成功后退出并跳转到登陆页
  * */
-$('#repass-form').on('submit', function() {
+$('#repass-form').ajaxForm({
 
-  // 校验
-  var newPass = $('#input-pass').val();
-  var newPassRepeat = $('#input-pass-repeat').val();
-  if(newPass !== newPassRepeat) {
-    alert('两次密码输入不一致');
-    return false;
-  }
+  // 提交前做密码校验
+  beforeSubmit: function(arr, $form, options) {
+    var newPass = $('#input-pass').val();
+    var newPassRepeat = $('#input-pass-repeat').val();
+    if(newPass !== newPassRepeat) {
+      alert('两次密码输入不一致');
+      return false;
+    }
+  },
 
-  // ajax提交，成功后触发头部的退出功能
-  $(this).ajaxSubmit(function(data) {
+  // 提交成功后给出用户提示，并退出登陆
+  success: function(data) {
     if(data.code == 200) {
       alert('密码修改成功');
       $('#btn-logout').trigger('click');
     }
-  });
-
-  return false;
+  }
 });
+
