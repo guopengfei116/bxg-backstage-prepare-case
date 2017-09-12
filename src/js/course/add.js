66c7a31b9efca5b671653afc3cab7717ecc1cd8c
PengFei <626138959@qq.com>
2017-09-12 15:59:26 +0800

    课程列表与添加

--- src/js/course/add.js
+++ src/js/course/add.js
@@ -1,1 +1,15 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+
+/**
+ * 表单提交：
+ * 1、使用插件ajaxForm方法监听表单submit事件并自动转ajax提交
+ * 2、成功后跳转到edit1页
+ * */
+$('#course-add-form').ajaxForm(function(data) {
+  if(data.code == 200) {
+    alert('添加成功');
+    location.href = '/dist/html/course/edit1.html?cs_id=' + data.result.cs_id;
+  }
+});
--- src/js/course/list.js
+++ src/js/course/list.js
@@ -1,1 +1,14 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+
+/**
+ * 渲染课程列表：
+ * 1、请求接口获取数据
+ * 2、通过模版引擎获取数据渲染后的模版，然后插入到页面指定位置
+ * */
+$.get('/v6/course', function(data) {
+  if(data.code === 200) {
+    $('.courses').html(template('course-list-tpl', data.result));
+  }
+});
