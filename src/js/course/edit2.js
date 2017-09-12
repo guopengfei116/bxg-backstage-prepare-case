586a855c1af357e6f2b2a01ba14acde9e5e9d8cb
PengFei <626138959@qq.com>
2017-09-13 03:38:58 +0800

    课程编辑第二步与第三步数据回显

--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -1,1 +1,25 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+var util = require('../common/util.js');
+
+/**
+ * 功能点：
+ * 1、数据回显
+ * 2、上传图片
+ * 3、裁剪图片
+ * */
+var cs_id = util.parseSearch('cs_id');
+
+/**
+ * 数据回显：
+ * 1、获取location.search中cs_id的值
+ * 2、调用接口获取数据
+ * 3、获取模版引擎数据渲染后的模版，插入指定元素
+ * */
+$.get('/v6/course/picture', { cs_id: cs_id }, function(data) {
+  if(data.code == 200) {
+    data.result.editIndex = 2;
+    $('.course-edit').append(template('course-edit2-tpl', data.result));
+  }
+});
--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -1,1 +1,26 @@
-require('../common/common.js');
+require('../common/loading.js');
+require('../common/header.js');
+require('../common/aside.js');
+var util = require('../common/util.js');
+
+/**
+ * 功能点：
+ * 1、数据回显
+ * 2、编辑课时
+ * 3、添加课时
+ * 4、删除课时
+ * */
+var cs_id = util.parseSearch('cs_id');
+
+/**
+ * 数据回显：
+ * 1、获取location.search中cs_id的值
+ * 2、调用接口获取数据
+ * 3、获取模版引擎数据渲染后的模版，插入指定元素
+ * */
+$.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
+  if(data.code == 200) {
+    data.result.editIndex = 3;
+    $('.course-edit').append(template('course-edit3-tpl', data.result));
+  }
+});
